55d108ba48006c7d87ea3155b583a7d64c82ac7d
duongthao0792 <69283500+duongthao0792@users.noreply.github.com>
2021-06-04 14:12:12 +0700

    Create shortlycontent.js

---
+++ shortlycontent.js
@@ -0,0 +1,38 @@
+function shortlyContent(){let data=[{title:`Instant Pot Duo 7-in-1 Electric Pressure Cooker`,content:`
+		- Duo, the number 1 selling multi-cooker, combines 7 kitchen appliances in 1. Pressure Cooker, Slow Cooker, Rice Cooker, Steamer, Sauté, Yogurt Marker and Warmer. Prepares dishes up to 70% faster to support your busy lifestyle.<br/>
+		- Features 14 Smart Programs – Soup, Meat/Stew, Bean/Chili, Poultry, Sauté/Simmer, Rice, Multigrain, Porridge, Steam, Slow Cook, Keep Warm, Yogurt, Manual, and Pressure Cook. Now, your favorite dishes are as easy as pressing a button<br/>
+		- Healthy, stainless steel (18/8) inner cooking pot made from food grade 304, no chemical coating, 3-ply bottom for even heat distribution. Fully sealed environment traps the flavours, nutrients and aromas within the food<br/>
+		- Built with the latest 3rd generation technology, the microprocessor monitors pressure, temperature, keeps time, and adjusts heating intensity and duration to achieve your desired results every time.<br/>
+		- UL and ULC certified. With 10 safety mechanisms to provide you with added assurance, designed to eliminate many common errors<br/>
+		- Power supply: 120V – 60Hz<br/>
+		<div class="short-img"><img src="https://1.bp.blogspot.com/-MWw7WGwLzcI/YBZkvBBA8OI/AAAAAAAABoI/wxopuyheqzE1TfA1X5v37HVwIqhSQbK9wCLcBGAsYHQ/s574/4.jpg"/></div>
+		<br/>
+		<h3>Pros And Cons</h3>
+		<b>+ Pros</b><br/>
+		- Safe<br/>
+		- Easy to use<br/>
+		- Lots of features<br/>
+		- Convenient<br/>
+		<b>+ Cons</b><br/>
+		- Can be picky, like the amount of liquid<br/>
+		- Eventually having to replace seal<br/>
+	`},{title:`Dash Deluxe Electric Air Fryer + Oven Cooker`,content:`
+		- HEALTHIER FRIED FOOD: Dash deluxe air fryer uses air crisp technology (instead of oil) to help reduce added fat by 70-80%, without sacrificing the flavor of your fried food (no more residual smell hours after you’ve eaten)<br/>
+		- 6 QUART CAPACITY: The PERFECT appliance for picky eaters, large families, entertaining, or those who have busy schedules. 6qt basket is larger, and makes an abundance of appetizers, desserts, chicken wings, french fries, even baked goods, and all within minutes of setting the timer – it couldn’t be easier!<br/>
+		- QUICK + EASY: Short on time? Simply load the large 6 qt capacity fryer basket, set the temp, and set the timer. Your food comes out crispy, guaranteed, EVERY TIME. And in less time than it takes to pre-heat your oven, making it more energy efficient.<br/>
+		- SAFER: The auto-shut off function prevents overcooking, and the cool touch housing and handle makes it safer to use. It’s so simple, even your kids can use it!<br/>
+		- The nonstick fryer basket is dishwasher safe for even easier cleanup!<br/>
+		- INCLUDES: The 1700-watt Deluxe air fryer is backed by a 1-year manufacturer warranty and includes a recipe book and recipe database access (all non-electric parts are dishwasher safe).<br/>
+		- Available in Black, White, Red, and Aqua.<br/>
+		<div class="short-img"><img src="https://1.bp.blogspot.com/-_BAJmj8ZPIQ/YBlmIx-TGdI/AAAAAAAABos/Yt8aCMZDST0MLjA1oben4GdL3J-dP3DlACLcBGAsYHQ/s600/3.jpg"/></div>
+		<br/>
+		<h3>Pros And Cons</h3>
+		<b>+ Pros</b><br/>
+		- Easy to Use<br/>
+		- Healthier<br/>
+		- Easy to Clean<br/>
+		- Less Power than an Oven<br/>
+		<b>+ Cons</b><br/>
+		- Slow to Cook<br/>
+	`}]
+return data;}
