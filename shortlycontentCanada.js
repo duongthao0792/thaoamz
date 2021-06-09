fce5e2b951a35eef2bb84f8d10893af93380ecc4
duongthao0792 <69283500+duongthao0792@users.noreply.github.com>
2021-06-09 09:49:49 +0700

    Update shortlycontentCanada.js

--- shortlycontentCanada.js
+++ shortlycontentCanada.js
@@ -1,1 +1,32 @@
-
+function shortlyContent(){let data=[{title:`Instant Pot Duo 7-in-1 Electric Pressure Cooker`,content:`
+		- Duo, the number 1 selling multi-cooker, combines 7 kitchen appliances in 1. Pressure Cooker, Slow Cooker, Rice Cooker, Steamer, Sauté, Yogurt Marker and Warmer. Prepares dishes up to 70% faster to support your busy lifestyle.<br/>
+		- Features 14 Smart Programs – Soup, Meat/Stew, Bean/Chili, Poultry, Sauté/Simmer, Rice, Multigrain, Porridge, Steam, Slow Cook, Keep Warm, Yogurt, Manual, and Pressure Cook. Now, your favorite dishes are as easy as pressing a button<br/>
+		- Healthy, stainless steel (18/8) inner cooking pot made from food grade 304, no chemical coating, 3-ply bottom for even heat distribution. Fully sealed environment traps the flavours, nutrients and aromas within the food<br/>
+		- Built with the latest 3rd generation technology, the microprocessor monitors pressure, temperature, keeps time, and adjusts heating intensity and duration to achieve your desired results every time.<br/>
+		- UL and ULC certified. With 10 safety mechanisms to provide you with added assurance, designed to eliminate many common errors<br/>
+		- Power supply: 120V – 60Hz<br/>
+		<div class="short-img"><img src="https://1.bp.blogspot.com/-MWw7WGwLzcI/YBZkvBBA8OI/AAAAAAAABoI/wxopuyheqzE1TfA1X5v37HVwIqhSQbK9wCLcBGAsYHQ/s574/4.jpg"/></div>
+		<br/>
+		<h3>Pros And Cons</h3>
+		<b>+ Pros</b><br/>
+		- Safe<br/>
+		- Easy to use<br/>
+		- Lots of features<br/>
+		- Convenient<br/>
+		<b>+ Cons</b><br/>
+		- Can be picky, like the amount of liquid<br/>
+		- Eventually having to replace seal<br/>
+	`},{title:`Arctic Dreams 10" Hybrid Cooling Gel Mattress`,content:`
+	<h3>Review of the Arctic Dreams 10" hybrid cooling gel mattress.</h3><br/>
+- This is a sensibly valued mattress, however, it turns out extraordinary for the individuals who experience difficulty resting or awakening around evening time.<br/>
+- The Arctic Dreams 10" crossover cooling gel bedding is reasonably delicate to help the dozing individual feel generally great and effectively nod off. With perfection and airiness, it assists you with having a profound and lively night's rest the following morning. This is a mattress that offers a lot of benefits for your health.<br/> 
+<div class="short-img"><img src="https://1.bp.blogspot.com/-kL6DhEBhYhE/YG6h2P2-8mI/AAAAAAAAAFA/Vwv2NAhz11ctw4sKPWqWcGJ2azAdrJUzgCLcBGAsYHQ/w443-h443/81ONvfT9zDL._AC_SL1500_.jpg"/></div>
+		<br/>
+<h3>Description of the Dreams 10" hybrid cooling gel mattress.</h3><br/>
+- Offered in an all-inclusive medium solace level, the Arctic Dreams is built with a .5" super delicate knitted top for rich, breathable solace. The top is intended to oppose body impressions over the long run.<br/> 
+- A protected 2.5" layer of Energex froth overcomes any issues among a latex, joining all the responsiveness of latex with the pressing factor point alleviation of adaptive padding.<br/>
+- A designed layer for better reactivity, pressure point helps, and movement detachment.<br/>
+- Compacted and moved for simple and helpful package transportation and set-up. If it's not too much trouble, permit as long as 24 hours for your sleeping pad to extend and likely scent to disseminate.<br/> `
+  }
+]
+return data;}
