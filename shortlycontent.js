daadca7dc866baa82287423bb7d44c094d0c07ee
duongthao0792 <69283500+duongthao0792@users.noreply.github.com>
2021-06-16 17:03:39 +0700

    Update shortlycontent.js

--- shortlycontent.js
+++ shortlycontent.js
@@ -31,6 +31,8 @@ function shortlyContent(){let data=[{title:`Dash Deluxe Electric Air Fryer + Ove
 <h3>Feeling when utilizing.</h3><br/>
 From the outset, I affirmed this sleeping pad isn't appropriate for individuals who like delicate pads like my mother. I like the smooth inclination, however not very delicate and indented, that Leesa bedding has. I am quite tall with weight and I like to rest on my back, my waking inclination is truly agreeable, I need to say I truly like it. I gave moving a shot at this pad and discovered it truly strong. There are a lot of pads when you rest on your back that will be exceptionally hot on the back which can make you sweat however with the Leesa mattress by no means having that feeling, I have been extremely fulfilled since possessing this sleeping pad, It truly changed my rest.
 <br/>
+<div class="short-img"><img src="https://1.bp.blogspot.com/-wvgCbCuRtIU/YDtDV4qfxLI/AAAAAAAAAO0/Uh2HheSyg0EHkNJQpGesDthgOvD2ENQzgCLcBGAsYHQ/w442-h442/bearmatress.jpg"/></div>
+		<br/>
 <h3>About highlights.</h3><br/>
 - This Leesa mattress is comprised of 3 layers of dynamic froth, I can see while eliminating their cover.<br/>
 - Cooling solace: Our restrictive great froth's microcell structure advances wind stream while as yet giving the ideal measure of embrace and ricochet. Along these lines, you rest cooler and get the solace and backing you need.<br/>
