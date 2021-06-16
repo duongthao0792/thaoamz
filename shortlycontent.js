db5084a2a3cebed6d2af53c420d27317136f862c
duongthao0792 <69283500+duongthao0792@users.noreply.github.com>
2021-06-16 17:00:45 +0700

    Update shortlycontent.js

--- shortlycontent.js
+++ shortlycontent.js
@@ -1,22 +1,4 @@
-function shortlyContent(){let data=[{title:`Instant Pot Duo 7-in-1 Electric Pressure Cooker`,content:`
-		- Duo, the number 1 selling multi-cooker, combines 7 kitchen appliances in 1. Pressure Cooker, Slow Cooker, Rice Cooker, Steamer, Sauté, Yogurt Marker and Warmer. Prepares dishes up to 70% faster to support your busy lifestyle.<br/>
-		- Features 14 Smart Programs – Soup, Meat/Stew, Bean/Chili, Poultry, Sauté/Simmer, Rice, Multigrain, Porridge, Steam, Slow Cook, Keep Warm, Yogurt, Manual, and Pressure Cook. Now, your favorite dishes are as easy as pressing a button<br/>
-		- Healthy, stainless steel (18/8) inner cooking pot made from food grade 304, no chemical coating, 3-ply bottom for even heat distribution. Fully sealed environment traps the flavours, nutrients and aromas within the food<br/>
-		- Built with the latest 3rd generation technology, the microprocessor monitors pressure, temperature, keeps time, and adjusts heating intensity and duration to achieve your desired results every time.<br/>
-		- UL and ULC certified. With 10 safety mechanisms to provide you with added assurance, designed to eliminate many common errors<br/>
-		- Power supply: 120V – 60Hz<br/>
-		<div class="short-img"><img src="https://1.bp.blogspot.com/-MWw7WGwLzcI/YBZkvBBA8OI/AAAAAAAABoI/wxopuyheqzE1TfA1X5v37HVwIqhSQbK9wCLcBGAsYHQ/s574/4.jpg"/></div>
-		<br/>
-		<h3>Pros And Cons</h3>
-		<b>+ Pros</b><br/>
-		- Safe<br/>
-		- Easy to use<br/>
-		- Lots of features<br/>
-		- Convenient<br/>
-		<b>+ Cons</b><br/>
-		- Can be picky, like the amount of liquid<br/>
-		- Eventually having to replace seal<br/>
-	`},{title:`Dash Deluxe Electric Air Fryer + Oven Cooker`,content:`
+function shortlyContent(){let data=[{title:`Dash Deluxe Electric Air Fryer + Oven Cooker`,content:`
 		- HEALTHIER FRIED FOOD: Dash deluxe air fryer uses air crisp technology (instead of oil) to help reduce added fat by 70-80%, without sacrificing the flavor of your fried food (no more residual smell hours after you’ve eaten)<br/>
 		- 6 QUART CAPACITY: The PERFECT appliance for picky eaters, large families, entertaining, or those who have busy schedules. 6qt basket is larger, and makes an abundance of appetizers, desserts, chicken wings, french fries, even baked goods, and all within minutes of setting the timer – it couldn’t be easier!<br/>
 		- QUICK + EASY: Short on time? Simply load the large 6 qt capacity fryer basket, set the temp, and set the timer. Your food comes out crispy, guaranteed, EVERY TIME. And in less time than it takes to pre-heat your oven, making it more energy efficient.<br/>
@@ -45,5 +27,14 @@ function shortlyContent(){let data=[{title:`Instant Pot Duo 7-in-1 Electric Pres
 - A protected 2.5" layer of Energex froth overcomes any issues among a latex, joining all the responsiveness of latex with the pressing factor point alleviation of adaptive padding.<br/>
 - A designed layer for better reactivity, pressure point helps, and movement detachment.<br/>
 - Compacted and moved for simple and helpful package transportation and set-up. If it's not too much trouble, permit as long as 24 hours for your sleeping pad to extend and likely scent to disseminate.<br/> `}
+,{title:`Leesa Mattress`,content:`
+<h3>Feeling when utilizing.</h3><br/>
+From the outset, I affirmed this sleeping pad isn't appropriate for individuals who like delicate pads like my mother. I like the smooth inclination, however not very delicate and indented, that Leesa bedding has. I am quite tall with weight and I like to rest on my back, my waking inclination is truly agreeable, I need to say I truly like it. I gave moving a shot at this pad and discovered it truly strong. There are a lot of pads when you rest on your back that will be exceptionally hot on the back which can make you sweat however with the Leesa mattress by no means having that feeling, I have been extremely fulfilled since possessing this sleeping pad, It truly changed my rest.
+<br/>
+<h3>About highlights.</h3><br/>
+- This Leesa mattress is comprised of 3 layers of dynamic froth, I can see while eliminating their cover.<br/>
+- Cooling solace: Our restrictive great froth's microcell structure advances wind stream while as yet giving the ideal measure of embrace and ricochet. Along these lines, you rest cooler and get the solace and backing you need.<br/>
+- Can be utilized with a bed establishment, stage bed, or flexible base<br/>
+* Those are my reviews of the Leesa mattress, if you have any questions about this product you can leave them in the comments below.<br/>`}
 					   ]
 return data;}
