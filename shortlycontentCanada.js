efc85acb7b489455b87247c8e29c027d0668b788
duongthao0792 <69283500+duongthao0792@users.noreply.github.com>
2021-06-19 11:08:33 +0700

    Update shortlycontentCanada.js

--- shortlycontentCanada.js
+++ shortlycontentCanada.js
@@ -1,22 +1,4 @@
-function shortlyContent(){let data=[{title:`Instant Pot Duo 7-in-1 Electric Pressure Cooker`,content:`
-		- Duo, the number 1 selling multi-cooker, combines 7 kitchen appliances in 1. Pressure Cooker, Slow Cooker, Rice Cooker, Steamer, Sauté, Yogurt Marker and Warmer. Prepares dishes up to 70% faster to support your busy lifestyle.<br/>
-		- Features 14 Smart Programs – Soup, Meat/Stew, Bean/Chili, Poultry, Sauté/Simmer, Rice, Multigrain, Porridge, Steam, Slow Cook, Keep Warm, Yogurt, Manual, and Pressure Cook. Now, your favorite dishes are as easy as pressing a button<br/>
-		- Healthy, stainless steel (18/8) inner cooking pot made from food grade 304, no chemical coating, 3-ply bottom for even heat distribution. Fully sealed environment traps the flavours, nutrients and aromas within the food<br/>
-		- Built with the latest 3rd generation technology, the microprocessor monitors pressure, temperature, keeps time, and adjusts heating intensity and duration to achieve your desired results every time.<br/>
-		- UL and ULC certified. With 10 safety mechanisms to provide you with added assurance, designed to eliminate many common errors<br/>
-		- Power supply: 120V – 60Hz<br/>
-		<div class="short-img"><img src="https://1.bp.blogspot.com/-MWw7WGwLzcI/YBZkvBBA8OI/AAAAAAAABoI/wxopuyheqzE1TfA1X5v37HVwIqhSQbK9wCLcBGAsYHQ/s574/4.jpg"/></div>
-		<br/>
-		<h3>Pros And Cons</h3>
-		<b>+ Pros</b><br/>
-		- Safe<br/>
-		- Easy to use<br/>
-		- Lots of features<br/>
-		- Convenient<br/>
-		<b>+ Cons</b><br/>
-		- Can be picky, like the amount of liquid<br/>
-		- Eventually having to replace seal<br/>
-	`},{title:`Arctic Dreams 10" Hybrid Cooling Gel Mattress`,content:`
+function shortlyContent(){let data=[{title:`Arctic Dreams 10" Hybrid Cooling Gel Mattress`,content:`
 	<h3>Review of the Arctic Dreams 10" hybrid cooling gel mattress.</h3><br/>
 - This is a sensibly valued mattress, however, it turns out extraordinary for the individuals who experience difficulty resting or awakening around evening time.<br/>
 - The Arctic Dreams 10" crossover cooling gel bedding is reasonably delicate to help the dozing individual feel generally great and effectively nod off. With perfection and airiness, it assists you with having a profound and lively night's rest the following morning. This is a mattress that offers a lot of benefits for your health.<br/> 
@@ -26,7 +8,41 @@ function shortlyContent(){let data=[{title:`Instant Pot Duo 7-in-1 Electric Pres
 - Offered in an all-inclusive medium solace level, the Arctic Dreams is built with a .5" super delicate knitted top for rich, breathable solace. The top is intended to oppose body impressions over the long run.<br/> 
 - A protected 2.5" layer of Energex froth overcomes any issues among a latex, joining all the responsiveness of latex with the pressing factor point alleviation of adaptive padding.<br/>
 - A designed layer for better reactivity, pressure point helps, and movement detachment.<br/>
-- Compacted and moved for simple and helpful package transportation and set-up. If it's not too much trouble, permit as long as 24 hours for your sleeping pad to extend and likely scent to disseminate.<br/> `
-  }
+- Compacted and moved for simple and helpful package transportation and set-up. If it's not too much trouble, permit as long as 24 hours for your sleeping pad to extend and likely scent to disseminate.<br/> 
+Buy it here on Amazon: https://amzn.to/3cAU64B <br/>
+`},{title:`SUPER DEAL 2IN1 Mini Compact Twin Tub Washing Machine`,content:`
+  <h3>Feel when utilizing.</h3><br/>
+ - First, this case was made of plastic but in my opinion, it was quite sturdy, I was able to move it to different positions very easily without fear of impact. In terms of washing ability, it is relatively clean, but note that there are areas that are too dirty like collars that may not be completely clean. In terms of volume, this machine completely meets the laundry needs of 2 adults and 1 small child in the family. If your family has more people, you really need to consider before buying. In terms of noise, in my opinion, it's not too loud.<br/>
+ <div class="short-img"><img src="https://1.bp.blogspot.com/-jjJrIEmGukM/YE8QpgqRANI/AAAAAAAAAPI/l0lUh9RFRuEDN-nfHimTOG9TzPBwVGW3wCLcBGAsYHQ/w441-h455/81ONvfT9zDL._AC_SL1500_.jpg"/></div>
+ <br/>
+ <h3>Feature.</h3><br/>
+ - Developed from high thickness plastic body + Aluminum pump + Upgraded engine makes working around for quite a long time. Simple to move and transport anywhere.<br/>
+ - Designed one sink for cleaning clothes and another for excess water flushing. This compact double drum washing machine is ideal for those wishing to do a lot of work, saving space.<br/>
+ - This honeycomb washer with a honeycomb inner drum can stack 11 lbs of clothes at once, big enough for a few things at once and in any case.<br/>
+ - The washing machine has separate clock control settings for washing machine and camcorder operations. The washing machine's clock for 15 minutes and the clock turns to 5 minutes per pile.<br/>
+Buy it here on Amazon: https://amzn.to/3pL9h0o <br/>
+`},{title:`Vornado VMH300 Whole Room Metal Heater`,content:`
+<h3>Feel when utilizing.</h3><br/>
+- I use it for my room and most rooms in the house. I can sleep well without having to wear a ton of clothes and cover myself with covers to forestall cold. I was surprised when I opened the entryway and left the room on account of the temperature contrast since it was very agreeable and didn't think the external would be that cold. This machine likewise permits me to change as indicated by the size of the room, yet I generally leave a similar model as I feel truly good. It can also be very easily transferred manually across different rooms however its price is also very modest so I have one per room.<br/>
+<div class="short-img"><img src="https://1.bp.blogspot.com/-toeaPMIRQKc/YDtKSjv0I1I/AAAAAAAAAO8/amH99ZP5W9YuyVa94X9nin8EduScA_6hACLcBGAsYHQ/w388-h447/bearmatress.jpg"/></div>
+ <br/>
+ <h3>Feature.</h3><br/>
+ - Vornado VMH300 entire room metal radiator tenderly warmth all noticeable all around in a little to medium-sized room through vortex air dissemination without utilizing serious warmth, like how a constrained air heater functions, simply on a more limited size.<br/>
+- Advanced wellbeing highlights incorporate a cool-contact steel case, tip-over insurance, and programmed security shut-off framework for effortless use.<br/>
+- Set your solace with 2 calm warmth settings (Low/750W, High/1500W) and a movable indoor regulator to tailor heat yield and energy utilization.<br/>
+Buy it here on Amazon: https://amzn.to/3ggVBqJ <br/>
+`},{title:`Mealthy MultiPot 9-in-1 Programmable Pressure Cooker`,content:`
+<h3>Experience when utilizing.</h3><br/>
+- First, actually, I should consider it a multi-work pot is more appropriate than a pressure cooker since it can cook a wide assortment of dishes. Notwithstanding the primary capacity of preparing food as a pressure cooker, Mealthy MultiPot pressure cooker can likewise cook rice, steam, sanitize, make soup, yogurt, and numerous different dishes. Actually an excessive amount of usefulness for a pressure cooker. <br/>
+- Do you regularly get shouted by kids, so on the grounds that you are eager when the food isn't prepared, it was very irritating around then, correct? The Mealthy MultiPot pressure cooker will take care of that issue, it can stew and cook dishes incredibly rapidly. The explanation I will make reference to in the segment underneath. <br/>
+- The Mealthy MultiPot pressure cooker truly changed my kitchen and can say it transformed me, extraordinary with straightforward, simple, and fast preparing to have delightful food. <br/>
+- Note: I have a note for you here that you should deliberately peruse the guidance manual to have the option to deliver pressing factor and open the pot securely. <br/>
+<div class="short-img"><img src="https://1.bp.blogspot.com/-Wc3jkweIGjY/YDX8ahn5nkI/AAAAAAAAAOk/JaKpQb1ejAoOb6joybtj6h7UzcWEeTN7QCLcBGAsYHQ/w426-h437/bearmatress.jpg"/></div>
+ <br/>
+<h3>Highlight .</h3><br/>
+- I have utilized many various kinds of pressing factor cookers and as I would see it this pressing factor cooker is very simple to utilize. With various catches on the front to allow you to pick cooking mode, temperature, and cooking time.<br/> 
+- The pot will consequently bolt the temperature and pressing factor when I close the cover, the warmth dispersion framework equitably in the pot helps the food cook and cook uniformly. <br/>
+- Regarding the material, the pot is made of tempered steel, there is a ton of hardened steel furniture however inevitably there are as yet rust checks yet with this pot I have utilized it for quite a while and it is as yet flawless,I appreciate for that. <br/>
+`}
 ]
 return data;}
