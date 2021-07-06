4c32b1718dcc3f2eb44ffd0a3bf65658056d7c4f
duongthao0792 <69283500+duongthao0792@users.noreply.github.com>
2021-07-06 12:35:31 +0700

    Update taquocthang.js

--- taquocthang.js
+++ taquocthang.js
@@ -1,1 +1,14 @@
-
+function getData(){
+	const keysw = "www.taquocthang.com";
+	var keyAPI = (typeof keyhost != 'undefined') ? keyhost : document.location.host;
+	var dataAMZ = {};
+	if(keysw == keyAPI){
+		dataAMZ = {
+			B000058TJ3: {href: "https://google.com/3iOvUjh",nameProduct: `Intex Swim Center Family Inflatable Pool`,image: '',summary: ``,slug: ""},
+			B00005RHPD: {href: "https://fb.com/3cC29hw",nameProduct: `Bosch 1617EVSPK Wood Router Tool Combo Kit`,image: '',summary: ``,slug: ""},
+			B0000AMK5F: {href: "https://amazon.com/3grCGIT",nameProduct: `BOSTITCH Coil Roofing Nailer`,image: '',summary: ``,slug: ""},
+			
+		}
+	}
+	return dataAMZ;
+}
