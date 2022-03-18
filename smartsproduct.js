5719444d388438a654a7ac005c37e7d52f86773b
duongthao0792 <69283500+duongthao0792@users.noreply.github.com>
2022-03-18 13:36:48 +0700

    Update smartsproduct.js

--- smartsproduct.js
+++ smartsproduct.js
@@ -1,1 +1,12 @@
-
+function getData(){
+	const keysw = "www.smartsproduct.com";
+	var keyAPI = (typeof keyhost != 'undefined') ? keyhost : document.location.host;
+	var dataAMZ = {};
+	if(keysw == keyAPI){
+		dataAMZ = {
+			B000058TJ3: {href: "google.com",nameProduct: `Intex Swim Center Family Inflatable Pool`,image: '',summary: ``,slug: ""},
+			B00005RHPD: {href: "https://fb.com/3okM3ix",nameProduct: `Bosch 1617EVSPK Wood Router Tool Combo Kit`,image: '',summary: ``,slug: ""},
+		}
+	}
+	return dataAMZ;
+}
